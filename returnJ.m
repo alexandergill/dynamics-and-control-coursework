914763ce5c54651b80532aa661780fc716db61a3
Alexander Gill <aasg20@bath.ac.uk>
2019-11-11 16:10:53 +0000

    update functions

--- returnJ.m
+++ returnJ.m
@@ -12,4 +12,21 @@ function J=returnJ
 % DO NOT MOFIFY ABOVE THIS LINE!!!! YOUR CODE GOES BELOW THIS LINE.
 %--------------------------------------------------------------------------     
 
+%% get moments of inertia for each frame
+% I is a 3x3x6 array of moments of inertia. Each 3x3 element contains the
+% moments in the form:
+%
+%  Ixx  Ixy  Ixz
+%  Iyx  Iyy  Iyz
+%  Izx  Izy  Izz   (note that Izx = Ixz and Ixy = Iyx) 
+%
+I = zeros(3,3,6);
+
+% links 1, 4, and 6 are cylinders
+for i = [1 4 6]
+    Izz = 0.5 * m(i) * r(i).^2;
+end
+
+% links 2, 3, and 5 are cuboids
+
 J=zeros(4,4,6);                    % initialise pseudo inertia matrix array
