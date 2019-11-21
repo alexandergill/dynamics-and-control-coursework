055ab358a63cbe4cf2bb5980f07726150bbd566d
Alexander Gill <aasg20@bath.ac.uk>
2019-11-21 18:45:16 +0000

    implement Uijk function

--- computeUijk.m
+++ computeUijk.m
@@ -19,4 +19,44 @@ function U=computeUijk(T,i,j,k)
 % DO NOT MOFIFY ABOVE THIS LINE!!!! YOUR CODE GOES BELOW THIS LINE.
 %--------------------------------------------------------------------------      
 
-U=zeros(4,4);
+% initialise empty U matrix
+U = zeros(4);
+
+% return zero matrix under these conditions
+if i < j || i < k
+    return
+else
+    % initialise U to identity matrix so its value can be assigned through
+    % multiplication
+    U = eye(4);
+    
+    % get product of terms from 1 to i
+    for index = 1:i
+        thisTerm = T(:,:,index);
+        
+        % differentiate terms if this iteration is j or k
+        % differentiate twice if j = k
+        if index == j
+            thisTerm = derivative(thisTerm);
+        end
+        if index == k
+            thisTerm = derivative(thisTerm);
+        end
+        
+        % get product at this iteration
+        U = U * thisTerm;
+    end
+end
+end
+
+function dTdq = derivative(T)
+% derivative returns the derivative with respect to q of the T matrix
+% this is calculated by multiplying a matrix 'mat' with the T matrix
+
+    % create matrix and compute derivative
+    mat = [0 -1  0  0;...
+           1  0  0  0;...
+           0  0  0  0;...
+           0  0  0  0];
+    dTdq = mat * T;
+end
