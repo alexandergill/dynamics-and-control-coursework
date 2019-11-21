7ce7741a208750df595bd42ea42dbcdc82ba569e
Alexander Gill <aasg20@bath.ac.uk>
2019-11-21 19:24:50 +0000

    begin implementing computeG

--- computeG.m
+++ computeG.m
@@ -20,5 +20,41 @@ function G=computeG(T)
 % Test procedure: 
 % Results:        
 
+%% hard-coded parameters for this robot
+density = 7850;
+radii   = [0.02 NaN NaN 0.01 NaN 0.01];
+lengths = [0.3 0.2 0.03 0.2 0.03 0.1];
+thicknesses  = [NaN 0.01 0.03 NaN 0.03 NaN];
+depths = [NaN 0.03 0.03 NaN 0.03 NaN];
+masses = zeros(1, 6);
+% links 1, 4, and 6 are cylinders
+for i = [1 4 6]
+    masses(i) = density * pi * radii(i).^2 * lengths(i);
+end
+% links 2, 3, and 5 are cuboids
+for i = [2 3 5]
+    masses(i) = density * lengths(i) * thicknesses(i) * depths(i);
+end
+
+r = [0    -0.15 0;...
+     -0.1 0     0;...
+     0    0     0;...
+     0    0.1   0;...
+     0    0     0;...
+     0    0     -0.05];
+
+g = [0;0;9.81;0];
+%% summation of gravity terms
+
+G=[0;0;0;0;0;0];
+
+for n = 1:6
+    for i = 1:6
+        U = computeUij(T,i,n);
+        mi = masses(i);
+        ri = r(i,:);
+        G(n) = G(n) - (mi * g' * U * ri);
+    end
+end
 
 G=[0;0;0;0;0;0];
